97980e25106dc81a554b3d6939cc5cb685e49849
parisdolfman <parisdolfman@gmail.com>
2021-06-14 14:48:14 -0400

    Stubbing out controller for comments

--- charities-api/app/controllers/comments_controller.rb
+++ charities-api/app/controllers/comments_controller.rb
@@ -1,2 +1,23 @@
 class CommentsController < ApplicationController
+    before_action :set_comment, only: [:update, :destroy]
+
+    def index
+      comments = Comment.all
+      render json: comments
+    end
+
+    def create
+      comment = current_user.comments.build(comment_params)
+      if comment.save
+        render json: comment
+      else 
+        error = {
+            #error message
+        }
+        render json: error
+      end 
+    end
+    
+    
+
 end
