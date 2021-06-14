8150903f381462494b01e978addd3a3a985264e6
parisdolfman <parisdolfman@gmail.com>
2021-06-14 14:28:29 -0400

    Stubbing out charities controller

--- charities-api/app/controllers/charities_controller.rb
+++ charities-api/app/controllers/charities_controller.rb
@@ -1,2 +1,24 @@
 class CharitiesController < ApplicationController
+    before_action :set_charity, only: [:update, :destroy, :show]
+
+    def index
+        charities = Charity.all
+        render json: charities
+    end
+    
+    def show
+      render json: @charity, serializer: CharityShowSerializer
+    end
+    
+    def create
+      charity = current_user.charity.build(charity_params)
+      charity.category = Category.find_or_create_by(name: params[:category])
+      if charity.save
+        render json: charity
+      else 
+        error = {
+            #error message here
+        }
+    end 
+
 end
