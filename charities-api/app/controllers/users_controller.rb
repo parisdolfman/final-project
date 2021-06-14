d0c663618e5b0b568863d978ae1ddafb9168d790
parisdolfman <parisdolfman@gmail.com>
2021-06-14 15:27:30 -0400

    Stubbed out users controller

--- charities-api/app/controllers/users_controller.rb
+++ charities-api/app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+
+    def create
+      user = User.new(user_params)  
+      if user.save
+        session[:user_id] = user.id
+        render json: user
+      else 
+        render json: {error: 'Cannot create user'}
+
+    end
+
+    private
+
+    def user_params
+        params.permit(:name, :username, :password)
+
 end
