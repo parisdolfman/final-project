ba43166daaf6f6c940c07a69772b9f2fe50bd83b
parisdolfman <parisdolfman@gmail.com>
2021-06-14 15:19:33 -0400

    Finished stubbing out comments controller

--- charities-api/app/controllers/comments_controller.rb
+++ charities-api/app/controllers/comments_controller.rb
@@ -26,7 +26,25 @@ class CommentsController < ApplicationController
         render json: {error: 'Update unsuccessful'}
       end 
     end 
+
+    def destroy
+      charity = @comment.charity
+      if @comment.destroy 
+        render json: charity, serializer: CharityShowSerializer
+      else 
+        render json: {error: 'Could not delete comment'}
+      end 
+    end 
+
+    private
     
+    def set_comment
+      @comment = Comment.find_by(id: params[:id])
+    end
+    
+    def comment_params
+      params.permit(:description)
+    end 
     
 
 end
