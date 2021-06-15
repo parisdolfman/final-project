f8bb75af321e324a8d38967e3e4dae01db410ae9
parisdolfman <parisdolfman@gmail.com>
2021-06-15 11:43:17 -0400

    Adding routes

--- charities-api/config/routes.rb
+++ charities-api/config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
   resources :users
   resources :comments
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  post "login" to: "create user"
+  post "sign up" to: "create new user"
+  
+
 end
