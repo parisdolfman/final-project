6ed41bacc52f0f23e452a50012cc835f8600a6c4
parisdolfman <parisdolfman@gmail.com>
2021-06-15 13:57:05 -0400

    Stubbed sign up method

--- charities-client/src/actions/currentUser.js
+++ charities-client/src/actions/currentUser.js
@@ -15,5 +15,39 @@ export const login = (credentials) => {
             },
             body: JSON.stringify(credentials)
         })
+        .then(response => response.json())
+        .then(response => {
+            alert(response.error)
+        } else {
+            dispatch({
+            //set the current user
+            })
+        }
+        })
+        .catch(console.log)
+    }
+}
+
+export const signup = (credentials) = > {
+    return dispatch => {
+        return fetch(`${URL}/signup`, {
+            credentials: "include",
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body:JSON.stringify(credentials)
+        })
+        .then(response => response.json())
+        .then(response => {
+            if (response.error) {
+                alert(response.error) 
+                } else {
+                    dispatch({
+                        //take current user response and create new user
+                    })
+            }
+        }) 
+        .catch(console.log)
     }
 }
