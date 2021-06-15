c1643957cabda28d18d0c235893ebd5516f6b009
parisdolfman <parisdolfman@gmail.com>
2021-06-15 12:17:04 -0400

    Added route restrictions

--- charities-api/config/routes.rb
+++ charities-api/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :categories
-  resources :charities
-  resources :users
-  resources :comments
+  resources :categories, only: [:index]
+  resources :charities, only: [:index, :show, :create, :update, :destroy]
+  resources :users, only: [:create]
+  resources :comments, only: [:index, :create, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   post "/login", to: "sessions#create"
