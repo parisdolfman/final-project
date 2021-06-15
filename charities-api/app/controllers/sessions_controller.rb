bd69cd10bd8dc6acf131eb8007ba7e86a79b8854
parisdolfman <parisdolfman@gmail.com>
2021-06-15 11:40:49 -0400

    Stubbed out get user and destroy methods

--- charities-api/app/controllers/sessions_controller.rb
+++ charities-api/app/controllers/sessions_controller.rb
@@ -9,4 +9,21 @@ class SessionsController < ApplicationController
             render json: {error: "Incorrect username or password"}
         end 
     end 
+
+    def get_current_user
+      if logged_in?
+        render json: current_user
+      else
+        render json: {error: "You are not currently logged in"}
+    end 
+
+    def destroy
+      session.delete :username
+      if !session[:user_id]
+        render json: {notice: "Log out successful"}
+      else
+        render json: {error: "You have not logged out"}
+      end 
+    end 
+    
 end
