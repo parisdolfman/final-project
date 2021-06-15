e456e7756327352dc2edc73e3e83e9eeec0b0795
parisdolfman <parisdolfman@gmail.com>
2021-06-15 11:46:22 -0400

    Stubbing out routes

--- charities-api/config/routes.rb
+++ charities-api/config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   resources :comments
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  post "login" to: "create user"
+  post "login" to: "create user session"
   post "sign up" to: "create new user"
-  
+  post "logout" to: "session destroy"
 
 end
