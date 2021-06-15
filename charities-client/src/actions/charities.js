cbcd37930204be08cf2f984cd45e6af42455bfb2
parisdolfman <parisdolfman@gmail.com>
2021-06-15 14:44:05 -0400

    Stubbing out charity addition method

--- charities-client/src/actions/charities.js
+++ charities-client/src/actions/charities.js
@@ -28,3 +28,35 @@ export const getCharities = () => {
     }
 }
 
+export const addCharity = (charity) => {
+    return dispatch => {
+        const charityInfo = {
+            image: charity.image,
+            category: charity.category
+            name: charity.name
+        }
+        return fetch(CHARITY_URL, {
+            credentials: "include"
+            method: "POST",
+            headers: {
+                "content-type": "application/json",
+            },
+            body: JSON.stringify(charityInfo)
+        })
+        .then(response => response.json())
+        .then(charity => {
+            if (charity.error) {
+                alert(charity.error)
+            } else {
+                dispatch({
+                    //add a news charity}
+            })
+            // get current user
+          }
+        })
+        .catch((error) => {
+            console.log(error)
+        });
+    }
+}
+
