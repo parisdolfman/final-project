b636fa874df7399442616cf1fc31af7061f03fb2
parisdolfman <parisdolfman@gmail.com>
2021-06-15 15:19:54 -0400

    Stubbing out return for charity component

--- charities-client/src/components/Charity.js
+++ charities-client/src/components/Charity.js
@@ -3,5 +3,18 @@ import { deleteCharity } from '../actions/charities'
 import { NavLink } from 'react-router-dom'
 
 const Charity = props => {
-    
+
+    const onClick = () => {
+        props.deletecharity(props.name)
+    }
+
+    return (
+      <div className='card'>
+          <div className='card_side card_side--front'>
+              <img className='card_image' src={props.image}></img>
+          </div>
+      </div>
+    )
 }
+
+export default 
