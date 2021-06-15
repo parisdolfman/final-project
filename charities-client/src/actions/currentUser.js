28afae00e6cb6329a471ebf7907722cb2280ff95
parisdolfman <parisdolfman@gmail.com>
2021-06-15 13:16:53 -0400

    Adding login method

--- charities-client/src/actions/currentUser.js
+++ charities-client/src/actions/currentUser.js
@@ -2,3 +2,18 @@ import {
     SET_CURRENT_USER,
     CLEAR_CURRENT_USER
 } from '../actionTypes'
+
+const URL = 'http://localhost:3000'
+
+export const login = (credentials) => {
+    return dispatch => {
+        return fetch(`$URL/login`, {
+            credentials: "include",
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(credentials)
+        })
+    }
+}
