00d5457c6b7cd5fe9bddb0e2a3746270449f7927
parisdolfman <parisdolfman@gmail.com>
2021-06-15 12:38:37 -0400

    Added comment action types

--- charities-client/src/actionTypes/index.js
+++ charities-client/src/actionTypes/index.js
@@ -2,3 +2,9 @@ const GET_CHARITIES = "GET_CHARITIES"
 const ADD_CHARITY = "ADD_CHARITIES"
 const EDIT_CHARITY = "ADD_CHARITY"
 const DELETE_CHARITY = "DELETE_CHARITY"
+//need a set or undo kind of action type for charity?
+
+const COMMENT_FORM = "COMMENT_FORM"
+const EDIT_COMMENT = "EDIT_COMMENT"
+const DELETE_COMMENT= "DELETE_COMMENT"
+
