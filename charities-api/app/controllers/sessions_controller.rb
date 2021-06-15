124db373f8b9dc7f80ec56329db7747004775ba4
parisdolfman <parisdolfman@gmail.com>
2021-06-15 11:38:06 -0400

    Added create method

--- charities-api/app/controllers/sessions_controller.rb
+++ charities-api/app/controllers/sessions_controller.rb
@@ -3,5 +3,10 @@ class SessionsController < ApplicationController
     def create
         user = User.find_by(username: params[:session][:username])
         if user && user.authenticate(params[:session][:password])
+            session[:user_id] = user.id
+            render json: user
+        else 
+            render json: {error: "Incorrect username or password"}
+        end 
     end 
 end
