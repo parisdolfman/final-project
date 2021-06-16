db59b85fe7b156e9746e1c59476c3a18dbb823f5
parisdolfman <parisdolfman@gmail.com>
2021-06-16 13:44:28 -0400

    Added authUser and logout methods

--- charities-client/src/actions/currentUser.js
+++ charities-client/src/actions/currentUser.js
@@ -1,6 +1,8 @@
 import {
     SET_CURRENT_USER,
-    CLEAR_CURRENT_USER
+    CLEAR_CURRENT_USER,
+    BEGIN_AUTH,
+    COMPLETE_AUTH
 } from '../actionTypes'
 
 const URL = 'http://localhost:3000'
@@ -77,6 +79,48 @@ export const getCurrentUser = () => {
             }
         })
         .catch(console.log)
+    }
+}
+
+export const authUser = () => {
+    return dispatch => {
+        dispatch({type: BEGIN_AUTH})
+        return fetch(`${URL}/get_current_user`, {
+            credentials: "include",
+            method: "GET",
+            headers: {
+                'Content-Type': "application/json"
+            },
+        })
+        .then(response => response.json())
+        .then(response => {
+            if (response.error) {
+                console.log(error);
+                dispatch({type: COMPLETE_AUTH})
+                dispatch({type: CLEAR_CURRENT_USER})
+            } else {
+                dispatch({
+                    type: SET_CURRENT_USER,
+                    user: response
+                })
+                dispatch({type: COMPLETE_AUTH})
+            }
+        })
+        .catch(console.log)
+    }
+}
+
+export const logout = (history) = > {
+    return dispatch => {
+        return fetch(`${URL}`, {
+            credentials: "include",
+            method: "DELETE"
+        })
+        .then(response => response.json())
+        .then(() => {
+            dispatch({type: CLEAR_CURRENT_USER})
+            history.push('/')
+        })
     }
 }
 
