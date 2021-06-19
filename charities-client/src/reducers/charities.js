04b2f67b946c8679ce8f620a6be4ababd948f6da
parisdolfman <parisdolfman@gmail.com>
2021-06-19 15:53:19 -0400

    Finished charities cases

--- charities-client/src/reducers/charities.js
+++ charities-client/src/reducers/charities.js
@@ -33,32 +33,67 @@ import {
     switch(action.type){
       case GET_CHARITIES:
         return {...state, charities: action.charities}
+
       case FILTERS_FORM_CHANGE:
         return {...state, filtersForm: {
           ...state.filtersForm, [action.payload.name]: action.payload.value
         }}
+
       case ADD_CHARITY:
         return {
           ...state,
           charities: [...state.charities, action.charities]
         }
+
       case EDIT_CHARITY:
         return {
           ...state, 
           charities: [...state.charities.map(charity => charity.id === action.charity.id ? action.charity : charity)]
         }
       
-        // case DELETE_CHARITY
-        // case SET_SELECTED_CHARITY
-        // case UNSET_CHARITY
-        // case COMMENT_FORM_UPDATE
-        // case SET_COMMENT
-        // case SET_FORM_DATA_EDIT_COMMENT
-        // case RESET_FORM_DATA_COMMENT
-        // case EDIT_COMMENT
-        // case DELETE_COMMENT
-        // case FILTER_FORM_CHANGE
+      case DELETE_CHARITY:
+        return {
+          ...state,
+          charities: [...state.charities.filter(charity => charity.id !== action.charityId)]
+        }
 
+      case SET_SELECTED_CHARITY:
+        return {...state, selectedCharity: action.charity}
 
+      case UNSET_CHARITY:
+        return {...state, selectedCharity: null}
+
+      case COMMENT_FORM_CHANGE:
+        return {...state, commentForm: {
+          ...state.commentForm, [action.payload.name]: action.payload.value
+        }}
+
+      case SET_COMMENT:
+        return {
+          ...state,
+          selectedCharity: {
+            ...state.selectedCharity,
+            comments: [...state.selectedCharity.comments, action.payload]
+          },
+          commentForm: nullCommentForm
+        }
+
+      case SET_FORM_DATA_EDIT_COMMENT:
+        return {...state, commentForm: {
+          ...state.commentForm, content: action.content
+        }}
+
+      case RESET_FORM_DATA_COMMENT:
+        return {...state, commentForm: nullCommentForm}
+
+      case EDIT_COMMENT:
+        return {...state, selectedCharity: action.charity,
+          commentForm: nullCommentForm
+        }
+
+      case DELETE_COMMENT:
+        return {...state, selectedCharity: action.charity}
+        default:
+          return state
     }
   }
