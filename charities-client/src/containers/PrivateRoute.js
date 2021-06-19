2497d9c0d69340bec85753e846140f129dd83639
parisdolfman <parisdolfman@gmail.com>
2021-06-19 11:13:00 -0400

    Stubbed private route for user to access My Profile

---
+++ charities-client/src/containers/PrivateRoute.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { getCharities } from '../actions/charities'
+import Charity from './Charity'
+import {withRouter} from 'react-router-dom'
+import CharityFormModal from './CharityFormModal'
+import Filters from './Filters'
+import { addCharity, editCharity } from '../actions/charities'
+import sprite from '../imgs/sprite.svg'
+
+
+class PrivateRoute extends Component {
+
+    componentDidMount() {
+      this.props.authUser()
+    }
+  
+    renderContent = props => {
+      const { Component, currentUser } = this.props
+      if (currentUser && currentUser.authBegin) {
+        if (currentUser && !currentUser.currentUser.username) {
+          return <Redirect to='/' />
+        } else {
+          return <Component {...props} />
+        }
+      }
+    }
+  
+    render() {
+      const { component: Component, currentUser, authUser } = this.props
+      return <Route {...rest} render={(props) => (
+        this.renderContent(props)
+      )} />
+    }
+  }
+  
+  const mapStateToProps = (state) => {
+    return { 
+      currentUser: state.currentUser 
+    }
+  }
+  
+  
+  export default connect(mapStateToProps, { authUser })(PrivateRoute)
