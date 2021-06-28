90bd6e82d14ec09333368f2d3f79d9f0e9fac402
parisdolfman <parisdolfman@gmail.com>
2021-06-28 12:01:50 -0400

    Corrected my profile page, now reflects properly

---
+++ charities-client/src/components/Profile.js
@@ -0,0 +1,82 @@
+import { getCurrentUser } from '../helpers';
+import React, { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+import { getAllCharities, getAllComments } from '../actions/charityActions';
+import CharityCard from './CharityCard';
+import { Card } from 'react-bootstrap';
+import {getCategories} from '../actions/charityActions'
+
+const Profile = ({ getCharities, charities, getAllCategories, getComments }) => {
+  const [profile, setProfile] = useState();
+
+  
+  const [userCharities, setuserCharities] = useState();
+
+  useState(() => {
+    getAllCategories()
+    const data = getCurrentUser();
+    getCharities();
+    getComments();
+    setProfile(data);
+
+  }, [profile]);
+
+
+
+  return profile ? (
+    <div>
+      <h3 className="text-center"> Name: {profile.name} </h3>
+
+      <h5 className="text-center"> Charities </h5>
+      <div className="d-flex justify-content-center flex-wrap items-center h-100 w-100">
+        {charities.charities ? (
+          charities.charities
+            .filter((ele) => ele.user_id == profile.id)
+            .map((charity) => (
+              <CharityCard key={charity.id} charity={charity} />
+            ))
+        ) : (
+          <h3> Loading... </h3>
+        )}
+      </div>
+      <h5 className="text-center"> Comments </h5>
+      <div className="w-50 mx-auto">
+        <div className="d-flex justify-content-center flex-column items-center h-100 w-100">
+          {charities.comments ? (
+            charities.comments
+              .filter((ele) => ele.user_id == profile.id)
+              .map((com, index) => (
+                <Card
+                  key={index.toString()}
+                  className="w-100 prof-comment-card mb-3"
+                >
+                  <Card.Header className="fw-light">
+                    Name: {com.username}
+                  </Card.Header>
+                  <Card.Body>
+                    <Card.Text className="fw-light">
+                      ({index + 1}) {com.description}
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              ))
+          ) : (
+            <h3> Loading </h3>
+          )}
+        </div>
+      </div>
+    </div>
+  ) : (
+    <p>Loading ... </p>
+  );
+};
+
+const mapStateToProps = (state) => ({ charities: state.charity });
+
+const mapDispatchToProps = {
+  getCharities: () => getAllCharities(),
+  getComments: () => getAllComments(),
+  getAllCategories: () => getCategories()
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Profile);
