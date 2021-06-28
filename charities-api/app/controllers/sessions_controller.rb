1bd122c92e018138e0b632c8d40f1d16d0be0d40
parisdolfman <parisdolfman@gmail.com>
2021-06-28 11:31:41 -0400

    Fixed sessions

--- charities-api/app/controllers/sessions_controller.rb
+++ charities-api/app/controllers/sessions_controller.rb
@@ -1,29 +1,31 @@
 class SessionsController < ApplicationController
     
-    def create
-        user = User.find_by(username: params[:session][:username])
-        if user && user.authenticate(params[:session][:password])
-            session[:user_id] = user.id
-            render json: user
-        else 
-            render json: {error: "Incorrect username or password"}
-        end 
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.password_digest === params[:session][:password_digest]
+        session[:user_id] = user.id
+        render json: user
+    else 
+        render json: {error: "Incorrect username or password"}, status: 403
     end 
+end 
 
-    def get_current_user
-      if logged_in?
-        render json: current_user
-      else
-        render json: {error: "You are not currently logged in"}
-    end 
+def get_current_user
+  if logged_in?
+    render json: current_user
+  else
+    render json: {error: "You are not currently logged in"}, status: 403
+  end
+end 
 
-    def destroy
-      session.delete :user_id
-      if !session[:user_id]
-        render json: {notice: "Log out successful"}
-      else
-        render json: {error: "You have not logged out"}
-      end 
-    end 
+def destroy
+  session.delete :user_id
+  if !session[:user_id]
+    render json: {notice: "Log out successful"}
+  else
+    render json: {error: "You have not logged out"}, status: 403
+  end 
+end 
+
 
 end
