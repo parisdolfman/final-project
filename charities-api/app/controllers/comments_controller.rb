4c0601c6fb47adc9b635501025d7c59bdc87a2ed
parisdolfman <parisdolfman@gmail.com>
2021-06-28 11:31:00 -0400

    Fixed comments controller

--- charities-api/app/controllers/comments_controller.rb
+++ charities-api/app/controllers/comments_controller.rb
@@ -1,47 +1,56 @@
 class CommentsController < ApplicationController
-    before_action :set_comment, only: [:update, :destroy]
+  before_action :set_comment, only: [:update, :destroy]
 
-    def index
-      comments = Comment.all
-      render json: comments
+  def index
+    comments = Comment.all
+    users = User.all
+    newComments = comments.map do |ele| 
+     
+        { id: ele.id, user_id: ele.user_id, description: ele.description, charity_id: ele.charity_id  ,username: ele.user_id ?  users.find_by(id: ele.user_id ).username : 'test user' }
     end
 
-    def create
-      comment = current_user.comments.build(comment_params)
-      if comment.save
-        render json: comment
-      else 
-        render json: {error: 'Creation unsuccessful'}
-      end 
-    end
+    render json: newComments
+  end
 
-    def update 
-      charity = @comment.charity
-      if @comment.update(description: params[:description])
-        render json: charity, serializer: CharityShowSerializer 
-      else 
-        render json: {error: 'Update unsuccessful'}
-      end 
+  def create
+    charity = Charity.find_by(id: params[:charity_id])
+    comment = charity.comments.build(comment_params)
+    if comment.save
+      render json: comment
+    else 
+      render json: {error: 'Creation unsuccessful'}, status: 403
     end 
+  end
 
-    def destroy
-      charity = @comment.charity
-      if @comment.destroy 
-        render json: charity, serializer: CharityShowSerializer
-      else 
-        render json: {error: 'Could not delete comment'}
-      end 
+  def update 
+    p params
+    p '____-----___-'
+    comment = Comment.find_by(id: params[:id])
+    if comment.update(description: params[:description])
+      render json: comment
+    else 
+      render json: {error: 'Update unsuccessful'}, status: 403
     end 
+  end 
 
-    private
-    
-    def set_comment
-      @comment = Comment.find_by(id: params[:id])
-    end
-    
-    def comment_params
-      params.permit(:description)
-    end 
-    
+  
+
+  def destroy
+    comment = Comment.find_by(id: params[:id])
+    comment.destroy
+    render json: {msg: 'Comment Removed'}
+  
+  end 
+
+  private
+  
+  def set_comment
+    @comment = Comment.find_by(id: params[:id])
+  end
+  
+  def comment_params
+    params.permit(:description, :user_id)
+  end 
+  
 
 end
