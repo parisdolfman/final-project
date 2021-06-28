6fd0f6fc36f365fcbb463cf92deafdb0ba2f5cd9
parisdolfman <parisdolfman@gmail.com>
2021-06-28 12:16:13 -0400

    Added corrected secure route for logging in

---
+++ charities-client/src/helpers/SecureRoute.js
@@ -0,0 +1,29 @@
+import { getCurrentUser } from './index';
+import { Redirect, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+
+
+const SecureRoute = (props) => {
+  const { component: Component, current_user, ...rest } = props;
+  const user = getCurrentUser();
+
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        user  ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="login" />
+        )
+      }
+    />
+  );
+};
+
+const mapStateToProps = (state) => ({
+  current_user: state.user,
+});
+
+export default connect(mapStateToProps)(SecureRoute);
