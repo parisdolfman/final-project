f72c2c4a2c39fd9837407975d61f2c1e86095d00
parisdolfman <parisdolfman@gmail.com>
2021-06-28 11:26:43 -0400

    Corrected index method

--- charities-api/app/controllers/charities_controller.rb
+++ charities-api/app/controllers/charities_controller.rb
@@ -2,8 +2,12 @@ class CharitiesController < ApplicationController
     before_action :set_charity, only: [:update, :destroy, :show]
 
     def index
-        charities = Charity.all
-        render json: charities
+      charities = Charity.all
+      users = User.all 
+      allcharities = charities.map  do |cha| 
+        {id: cha.id, user_id: cha.user_id, image: cha.image, category_id: cha.category_id, name: cha.name, username: cha.user_id ?  users.find_by(id: cha.user_id ).username : 'test user' }
+      end
+      render json: allcharities
     end
     
     def show
